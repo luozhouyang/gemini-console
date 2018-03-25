3bbb86bb3d1d3ef3cee401743388c5a49c933145
luozhouyang <stupidme.me.lzy@gmail.com>
2018-03-25 21:15:07 +0800

    Create token manager

--- app/src/main/java/me/stupidme/console/utils/TokenManager.java
+++ app/src/main/java/me/stupidme/console/utils/TokenManager.java
@@ -5,4 +5,37 @@ package me.stupidme.console.utils;
  */
 
 public class TokenManager {
+
+    private static volatile TokenManager sInstance;
+
+    private String mKey;
+    private static final String TOKEN = "";
+
+    private TokenManager() {
+
+    }
+
+    public static TokenManager getInstance() {
+        if (sInstance == null) {
+            synchronized (TokenManager.class) {
+                if (sInstance == null) {
+                    sInstance = new TokenManager();
+                }
+            }
+        }
+        return sInstance;
+    }
+
+    public void setKey(String key) {
+        mKey = key;
+    }
+
+    public String getToken() {
+        if (mKey == null || mKey.isEmpty()) {
+            LoggerProxy.w(TokenManager.class.getCanonicalName(), "Key is empty. You need " +
+                    "specify a key first.");
+            return "";
+        }
+        return SecurityUtil.decryptDES(TOKEN, mKey);
+    }
 }
