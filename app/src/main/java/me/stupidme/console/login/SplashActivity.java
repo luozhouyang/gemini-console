761df5002abb6810c71a959313048034e12a2a57
luozhouyang <stupidme.me.lzy@gmail.com>
2018-04-09 22:06:18 +0800

    Fixed login logic

--- app/src/main/java/me/stupidme/console/login/SplashActivity.java
+++ app/src/main/java/me/stupidme/console/login/SplashActivity.java
@@ -4,12 +4,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-import me.stupidme.console.main.MainActivity;
 import me.stupidme.console.R;
 import me.stupidme.console.account.Account;
 import me.stupidme.console.account.AccountManager;
 import me.stupidme.console.account.AccountManagerImpl;
+import me.stupidme.console.main.MainActivity;
 import me.stupidme.console.utils.LoggerProxy;
+import me.stupidme.stupidhttp.PostRequest;
+import me.stupidme.stupidhttp.Request;
+import me.stupidme.stupidhttp.RequestCallback;
+import me.stupidme.stupidhttp.StupidHttp;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -34,21 +38,28 @@ public class SplashActivity extends AppCompatActivity {
             turnToLoginActivity();
             return;
         }
-        LoginModel login = new LoginModelImpl();
-        boolean success = login.login(name, password);
-        if (!success) {
-            LoggerProxy.e(SplashActivity.class.getCanonicalName(),
-                    "Login failed.");
-            turnToLoginActivity();
-            return;
-        }
-        startActivity(new Intent(SplashActivity.this, MainActivity.class));
-        finish();
+
+        UserInfoItem infoItem = new UserInfoItem(name, password);
+        Request request = new PostRequest("http://127.0.0.1:8080", infoItem);
+        RequestCallback callback = new RequestCallback() {
+            @Override
+            public void onException(Exception e) {
+                LoggerProxy.e(SplashActivity.class.getCanonicalName(),
+                        "Login failed.");
+                turnToLoginActivity();
+            }
+
+            @Override
+            public void onSuccess(String response) {
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                finish();
+            }
+        };
+        StupidHttp.getInstance().go(request, callback);
     }
 
     private void turnToLoginActivity() {
         startActivity(new Intent(SplashActivity.this, LoginActivity.class));
         finish();
     }
-
 }
