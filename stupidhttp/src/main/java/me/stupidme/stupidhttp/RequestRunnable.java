633d7f77d611b01d3986a2cff16159ca605fa217
luozhouyang <stupidme.me.lzy@gmail.com>
2018-04-10 21:02:03 +0800

    Setting cookie

--- stupidhttp/src/main/java/me/stupidme/stupidhttp/RequestRunnable.java
+++ stupidhttp/src/main/java/me/stupidme/stupidhttp/RequestRunnable.java
@@ -1,9 +1,14 @@
 package me.stupidme.stupidhttp;
 
+import android.text.TextUtils;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.CookieManager;
+import java.net.HttpCookie;
 import java.net.HttpURLConnection;
+import java.util.List;
 
 /**
  * Created by allen on 18-4-9.
@@ -26,6 +31,11 @@ public class RequestRunnable implements Runnable {
         }
         try {
             HttpURLConnection connection = mRequest.getConnection();
+            CookieManager manager = new CookieManager();
+            if (manager.getCookieStore().getCookies().size() > 0) {
+                connection.setRequestProperty("Cookie",
+                        TextUtils.join(";", manager.getCookieStore().getCookies()));
+            }
             connection.connect();
 
             if (connection.getResponseCode() != HttpURLConnection.HTTP_OK) {
@@ -33,6 +43,12 @@ public class RequestRunnable implements Runnable {
                     mCallback.onException(new Exception("Bad response. Code: " + connection.getResponseCode()));
                 }
                 return;
+            }
+            List<String> cookies = connection.getHeaderFields().get("Set-Cookie");
+            if (cookies != null) {
+                for (String cookie : cookies) {
+                    manager.getCookieStore().add(null, HttpCookie.parse(cookie).get(0));
+                }
             }
 
             BufferedReader reader = new BufferedReader(
