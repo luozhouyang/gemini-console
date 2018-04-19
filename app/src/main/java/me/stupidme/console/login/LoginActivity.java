0c60e13a6ec0938c29f2a0bb92c454ebe43a2312
luozhouyang <stupidme.me.lzy@gmail.com>
2018-04-19 22:24:26 +0800

    Refactor login activity

--- app/src/main/java/me/stupidme/console/login/LoginActivity.java
+++ app/src/main/java/me/stupidme/console/login/LoginActivity.java
@@ -1,206 +1,107 @@
 package me.stupidme.console.login;
 
-import android.animation.Animator;
-import android.animation.AnimatorListenerAdapter;
-import android.annotation.TargetApi;
-import android.content.ContentValues;
 import android.content.Intent;
-import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
-import android.support.design.widget.TextInputEditText;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.text.TextUtils;
-import android.view.KeyEvent;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.view.inputmethod.EditorInfo;
-import android.widget.ArrayAdapter;
-import android.widget.AutoCompleteTextView;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
+import android.support.v7.widget.Toolbar;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
 
 import me.stupidme.console.R;
-import me.stupidme.console.account.UserInfoProvider;
 import me.stupidme.console.main.MainActivity;
-import me.stupidme.console.utils.UserNameHistory;
+import me.stupidme.console.utils.LoggerProxy;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements LoginFragment.OnLoginListener,
+        SignUpFragment.OnSignUpListener {
 
-    private AutoCompleteTextView mUserNameView;
-    private TextInputEditText mEmailView;
-    private EditText mPasswordView;
-    private View mProgressView;
-    private View mLoginFormView;
+    private ViewPager mViewPager;
+    private Toolbar mToolbar;
 
-    private LoginPresenter mPresenter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        // Set up the login form.
-        mUserNameView = (AutoCompleteTextView) findViewById(R.id.username);
-        addUserNamesToAutoComplete();
-
-        mPasswordView = (EditText) findViewById(R.id.password);
-        mPasswordView.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+        mToolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+        getWindow().setStatusBarColor(ContextCompat.getColor(this, R.color.colorPrimaryDark));
+        mViewPager = findViewById(R.id.view_pager);
+        List<BaseFragment> fragments = new ArrayList<>(2);
+        fragments.add(new LoginFragment());
+        fragments.add(new SignUpFragment());
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager(), fragments);
+        mViewPager.setAdapter(adapter);
+        mViewPager.setCurrentItem(0);
+        mViewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
             @Override
-            public boolean onEditorAction(TextView textView, int id, KeyEvent keyEvent) {
-                if (id == EditorInfo.IME_ACTION_DONE || id == EditorInfo.IME_NULL) {
-                    attemptLogin();
-                    return true;
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                mViewPager.setCurrentItem(position);
+                if (position == 0) {
+                    mToolbar.setTitle(getString(R.string.action_sign_in));
+                } else {
+                    mToolbar.setTitle(getString(R.string.action_sign_up));
                 }
-                return false;
-            }
-        });
-        mEmailView = findViewById(R.id.email);
-
-        Button mSignInButton = (Button) findViewById(R.id.sign_in_button);
-        mSignInButton.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                attemptLogin();
-            }
-        });
-
-        mLoginFormView = findViewById(R.id.login_form);
-        mProgressView = findViewById(R.id.login_progress);
-
-        mPresenter = new LoginPresenter(new LoginView() {
-            @Override
-            public void loginSuccess() {
-                String username = mUserNameView.getText().toString();
-                String email = mEmailView.getText().toString();
-                String password = mPasswordView.getText().toString();
-                UserNameHistory.getInstance()
-                        .addUserName(username)
-                        .commit(LoginActivity.this);
-                saveUserInfo(username, email, password);
-                showProgress(false);
-                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                startActivity(intent);
-                LoginActivity.this.finish();
             }
 
             @Override
-            public void loginFailed() {
-                showProgress(false);
-                mPasswordView.setError(getString(R.string.error_incorrect_password));
-                mPasswordView.requestFocus();
+            public void onPageScrollStateChanged(int state) {
+
             }
         });
     }
 
-    private void saveUserInfo(String username, String email, String password) {
-        Uri uri = UserInfoProvider.USER_INFO_URI;
-        ContentValues values = new ContentValues(3);
-        values.put("username", username);
-        values.put("email", email);
-        values.put("password", password);
-        getContentResolver().insert(uri, values);
+    @Override
+    public void onLoginSuccess() {
+        startActivity(new Intent(LoginActivity.this, MainActivity.class));
+        finish();
     }
 
-    private void attemptLogin() {
+    @Override
+    public void onLoginFailed(Exception e) {
+        Toast.makeText(this, "Login failed:" + e.getMessage(), Toast.LENGTH_SHORT)
+                .show();
+    }
 
-        // Reset errors.
-        mUserNameView.setError(null);
-        mPasswordView.setError(null);
-        mEmailView.setError(null);
+    @Override
+    public void onSignUpSuccess() {
+        LoggerProxy.w("LoginActivity", "Sign Up Success!");
+        mViewPager.setCurrentItem(0);
+    }
 
-        // Store values at the time of the login attempt.
-        String username = mUserNameView.getText().toString();
-        String email = mEmailView.getText().toString();
-        String password = mPasswordView.getText().toString();
+    @Override
+    public void onSignUpFailed(Exception e) {
+        LoggerProxy.w("LoginActivity", "Sign Up Failed!");
+    }
 
-        boolean cancel = false;
-        View focusView = null;
+    private static class ViewPagerAdapter extends FragmentPagerAdapter {
 
-        // Check for a valid password, if the user entered one.
-        if (!TextUtils.isEmpty(password) && !isPasswordValid(password)) {
-            mPasswordView.setError(getString(R.string.error_invalid_password));
-            focusView = mPasswordView;
-            cancel = true;
+        private List<BaseFragment> mFragments;
+
+        public ViewPagerAdapter(FragmentManager fm, List<BaseFragment> fragments) {
+            super(fm);
+            mFragments = fragments;
         }
 
-        // Check for a valid email address.
-        if (TextUtils.isEmpty(email)) {
-            mEmailView.setError(getString(R.string.error_field_required));
-            focusView = mEmailView;
-            cancel = true;
-        } else if (!isEmailValid(email)) {
-            mEmailView.setError(getString(R.string.error_invalid_username));
-            focusView = mEmailView;
-            cancel = true;
+        @Override
+        public Fragment getItem(int position) {
+            return mFragments.get(position);
         }
 
-        if (cancel) {
-            // There was an error; don't attempt login and focus the first
-            // form field with an error.
-            focusView.requestFocus();
-        } else {
-            // Show a progress spinner, and kick off a background task to
-            // perform the user login attempt.
-            showProgress(true);
-            mPresenter.login(username, email, password);
+        @Override
+        public int getCount() {
+            return mFragments.size();
         }
-    }
-
-    private boolean isEmailValid(String email) {
-        return email.contains("@");
-    }
-
-    private boolean isPasswordValid(String password) {
-        return password.length() > 4;
-    }
-
-    @TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
-    void showProgress(final boolean show) {
-        // On Honeycomb MR2 we have the ViewPropertyAnimator APIs, which allow
-        // for very easy animations. If available, use these APIs to fade-in
-        // the progress spinner.
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
-            int shortAnimTime = getResources().getInteger(android.R.integer.config_shortAnimTime);
-
-            mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
-            mLoginFormView.animate().setDuration(shortAnimTime).alpha(
-                    show ? 0 : 1).setListener(new AnimatorListenerAdapter() {
-                @Override
-                public void onAnimationEnd(Animator animation) {
-                    mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
-                }
-            });
-
-            mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
-            mProgressView.animate().setDuration(shortAnimTime).alpha(
-                    show ? 1 : 0).setListener(new AnimatorListenerAdapter() {
-                @Override
-                public void onAnimationEnd(Animator animation) {
-                    mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
-                }
-            });
-        } else {
-            // The ViewPropertyAnimator APIs are not available, so simply show
-            // and hide the relevant UI components.
-            mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
-            mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
-        }
-    }
-
-
-    private void addUserNamesToAutoComplete() {
-        //Create adapter to tell the AutoCompleteTextView what to show in its dropdown list.
-        Set<String> names = UserNameHistory.getInstance().getHistoryUserNames(this);
-        List<String> list = new ArrayList<>(names);
-        ArrayAdapter<String> adapter =
-                new ArrayAdapter<>(LoginActivity.this,
-                        android.R.layout.simple_dropdown_item_1line, list);
-        mUserNameView.setAdapter(adapter);
     }
 
 }
